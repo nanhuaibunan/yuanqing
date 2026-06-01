faf703ad6677b38f4a19ac5f45cdfb2cf0fc0796
nanhuaibunan <842815085@qq.com>
2026-06-01 17:16:26 +0800

    feat: 完成鸢青结单表处理系统

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repo = 'yuanqing'
+
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 })
